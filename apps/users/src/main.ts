ec68f29ff3585876acdd1d0932605f242a70e03e
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-09-04 20:40:01 +0300

    feat: add helmet (cookie security) and cors

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
-  app.useGlobalPipes(new ValidationPipe());
-
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors({
+    origin: configService.get<string>('CORS_ALLOWED_ORIGINS', '*'),
+  });
+
+  await app.listen(configService.get<number>('PORT', 3000));
 }
 bootstrap();
